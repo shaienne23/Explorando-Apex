5e3266a74f06a5cbef37e7822d272486a6c55dcf
shaienne23 <shaienneo@gmail.com>
2024-08-13 15:55:49 -0300

    fix: ajustes

--- force-app/main/default/aura/TicketManager/TicketManagerController.js
+++ force-app/main/default/aura/TicketManager/TicketManagerController.js
@@ -1,5 +1,10 @@
 ({
-    myAction : function(component, event, helper) {
+    init : function(component, event, helper) {
+        let aeroportoOrigem = 'ID';
+        let aeroportoDestino = 'ID';
+        let dataPartida = '2024-01-10';
+        let dataRetorno = '2024-02-10';
 
+        helper.loadVoos(component, aeroportoOrigem, aeroportoDestino, dataPartida, dataRetorno)
     }
 })
--- force-app/main/default/aura/TicketManager/TicketManagerHelper.js
+++ force-app/main/default/aura/TicketManager/TicketManagerHelper.js
@@ -1,5 +1,23 @@
 ({
-    helperMethod : function() {
+    loadVoos : function( component, aeroportoOrigem, aeroportoDestino, dataPartida, dataRetorno) {
+        let action = component.get('c.getVoos');
+        action.setParam('aeroportoOrigem', aeroportoOrigem);
+        action.setParam('aeroportoDestino', aeroportoDestino);
+        action.setParam('dataPartida', dataPartida);
+        action.setParam('dataRetorno', dataRetorno);
 
+        action.setCallback(this, function(result){
+            let state = result.getState();
+
+            if(state == 'SUCCESS'){
+                let voos = result.getReturnValue();
+                component.set('v.voos', voos);
+            }
+            else if ( state == 'ERROR') {
+                window.console.error(getError());
+            }
+        })
+        // para disparar a action usamos a palavra reservada passando a action como parametro
+       $A.enqueueAction(action);
     }
 })
